c913cff101acd03cbc62683fe4e2d01a34164337
ty <towyuan@outlook.com>
2022-05-11 12:06:59 +0800

    chore: remove @bleco/oso package for refactor

--- packages/oso/src/polar/types.ts
+++
@@ -1,8 +0,0 @@
-export interface PolarResource {
-  name: string;
-  permissions?: string[];
-  roles?: string[];
-  rolePermissions?: Record<string, string[]>;
-  roleInherits?: Record<string, string[]>;
-  relations?: Record<string, {model: string; property?: string} | string>;
-}
--- packages/oso/src/types.ts
+++
@@ -1,9 +0,0 @@
-import {Entity} from '@loopback/repository';
-import {QueryWhere} from '@bleco/query';
-
-export const OsoDataSourceName = 'OsoDB';
-
-export interface ResourceFilter<T extends Entity = Entity> {
-  model: string;
-  where: QueryWhere<T>;
-}
