ad7b7513e4e6ea6bb36c78b906656922d2c465bf
ty <towyuan@outlook.com>
2022-05-28 15:35:21 +0800

    chore: builtin bind DefaultRepositoryFactory as default RepositoryFactory

--- packages/acl-oso/src/__tests__/acceptances/oso-enforcer.acceptance.ts
+++ packages/acl-oso/src/__tests__/acceptances/oso-enforcer.acceptance.ts
@@ -1,15 +1,9 @@
 import {testEnforcerBatch} from '@bleco/acl/dist/test';
 import {OsoComponent} from '../../oso.component';
 import {spyOnAdapter} from '../support';
-import {DefaultRepositoryFactoryProvider, RepositoryFactoryBindings} from '@bleco/repository-factory';
-import {BindingScope} from '@loopback/core';
 
 describe('OsoEnforcer', function () {
   testEnforcerBatch(app => {
-    app
-      .bind(RepositoryFactoryBindings.REPOSITORY_FACTORY)
-      .toProvider(DefaultRepositoryFactoryProvider)
-      .inScope(BindingScope.SINGLETON);
     app.component(OsoComponent);
     spyOnAdapter(app);
   });
--- packages/acl-oso/src/oso.component.ts
+++ packages/acl-oso/src/oso.component.ts
@@ -1,13 +1,15 @@
+import {AclBindings} from '@bleco/acl';
 import {Application, Component, ContextTags, CoreBindings, inject, injectable, ProviderMap} from '@loopback/core';
+import {DefaultRepositoryFactoryProvider, RepositoryFactoryBindings} from '@bleco/repository-factory';
 import {OsoBindings} from './keys';
 import {OsoAliaser} from './alias';
-import {AclBindings} from '@bleco/acl';
 import {OsoEnforcerProvider} from './providers';
 
 @injectable({tags: {[ContextTags.KEY]: OsoBindings.COMPONENT.key}})
 export class OsoComponent implements Component {
   providers: ProviderMap = {
     [AclBindings.ENFORCER_STRATEGY.key]: OsoEnforcerProvider,
+    [RepositoryFactoryBindings.REPOSITORY_FACTORY.key]: DefaultRepositoryFactoryProvider,
   };
 
   constructor(
