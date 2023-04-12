f1b02bfcab2c427be095d01a8fe6d14f96657dab
TY <towyuan@outlook.com>
2023-04-12 19:10:43 +0800

    chore: upgrade dependencies

--- examples/oidp-server/src/config.ts
+++ examples/oidp-server/src/config.ts
@@ -1,14 +1,10 @@
-import {ConfigLoader} from '@bleco/config';
-import {PortablePath} from '@jil/ncommon';
+import {load} from '@bleco/config';
 import {ApplicationConfig} from '@loopback/core';
 
-// file name format: https://boostlib.dev/docs/config/#loading-config-files
-const configLoader = new ConfigLoader('oidp');
-
-export async function loadConfig(fromDir?: PortablePath): Promise<ApplicationConfig> {
+export async function loadConfig(fromDir?: string): Promise<ApplicationConfig> {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const defaults = require('../app.defaults.js');
-  const config = await configLoader.load(fromDir);
+  const config = await load('oidp', fromDir);
   return {...defaults, ...config};
 }
