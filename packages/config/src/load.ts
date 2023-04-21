df87f9d0d6b5e651b2d8ce3bb1a91228b10c16b9
TY <towyuan@outlook.com>
2023-04-21 16:18:23 +0800

    chore: fix blanks and nulls removal function

--- packages/config/src/load.ts
+++ packages/config/src/load.ts
@@ -60,31 +60,44 @@ export async function load<T extends object>(
   const opts = options ?? {};
   opts.fromDirs = opts.fromDirs ?? fromDirs;
   opts.mergeToProcessEnv = opts.mergeToProcessEnv ?? false;
-  const c = name instanceof Configuration ? name : new Config<T>(name);
-  const dirs = toArray(opts.fromDirs ?? process.cwd());
-  const {env} = Env.load(dirs, opts);
-  const config = await loadConfigs<T>(c, dirs, opts);
-  return popu(config, env ?? {}, options);
-}
 
-async function loadConfigs<T extends object>(c: Configuration<T>, fromDirs: string[], options: LoadOptions<T> = {}) {
-  const answer: AnyObj = {};
-  for (const dir of fromDirs) {
-    debug('load config from %s', dir);
-    const {config} = await c.loadConfigFromRoot(dir);
-    Object.assign(answer, config);
-  }
-  const result = merge(options.defaults ?? {}, answer);
-  if (!options.keepBlanks || !options.keepNulls) {
+  // create config
+  const c = name instanceof Configuration ? name : new Config<T>(name);
+
+  // get dirs
+  const dirs = toArray(opts.fromDirs ?? process.cwd());
+
+  // load envs from dirs
+  const {env} = Env.load(dirs, opts);
+
+  // load configs from dirs
+  const config = await loadConfigs<T>(c, dirs, opts.defaults);
+
+  // resolve variables
+  const result = popu(config, env ?? {}, options);
+
+  // remove blanks and nulls
+  const {keepBlanks, keepNulls} = opts;
+  if (!keepBlanks || !keepNulls) {
     traverse(result, ({parent, key, value, meta}) => {
       if (parent && key) {
-        if (!options.keepBlanks && value === '') {
+        if (!keepBlanks && value === '') {
           delete parent[key];
-        } else if (!options.keepNulls && value === null) {
+        } else if (!keepNulls && value === null) {
           delete parent[key];
         }
       }
     });
   }
   return result;
+}
+
+async function loadConfigs<T extends object>(c: Configuration<T>, fromDirs: string[], defaults?: Partial<T>) {
+  const answer: AnyObj = {};
+  for (const dir of fromDirs) {
+    debug('load config from %s', dir);
+    const {config} = await c.loadConfigFromRoot(dir);
+    Object.assign(answer, config);
+  }
+  return merge(defaults ?? {}, answer);
 }
