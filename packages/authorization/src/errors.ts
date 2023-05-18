460064bb35f40d70e288b0fc4b306b3ddcd9f206
TY <towyuan@outlook.com>
2023-05-18 18:56:05 +0800

    fix: AuthorizationErrors naming

--- packages/authorization/src/errors.ts
+++ packages/authorization/src/errors.ts
@@ -1,5 +1,5 @@
 import {BErrors} from 'berrors';
 
-export const AuthorizeErrors = {
+export const AuthorizationErrors = {
   NotAllowedAccess: BErrors.Forbidden.subclass('NotAllowedAccessError', 'Not allowed access', 'not_allowed_access'),
 };
